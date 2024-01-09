62606ba2e427df205e84098fbbf16bccda088186
vaecebyZ <vaeceby2@yandex.ru>
2024-01-09 13:26:11 +0800

    Add dependencies and configuration file

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,13 @@
  * @LastEditTime: 2024-01-04 12:24:53
  * @FilePath: \hostChan\src\main.rs
  * @Description: 
- * 
+ * Auto fetch hosts file.
  * Copyright (c) 2024 by vaecebyZ, All Rights Reserved. 
  */
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::Path;
+use config::{Config, File};
 use reqwest;
 
 async fn download_hosts_file(url: &str) -> Result<String, reqwest::Error> {
@@ -38,8 +39,15 @@ fn append_to_hosts_file(content: &str) -> std::io::Result<()> {
 
 #[tokio::main]
 async fn main() {
-    let url = "https://hosts.sanka.sbs";
-    match download_hosts_file(url).await {
+    let mut config = Config::new();
+    config.merge(File::with_name("config/config.toml")).unwrap();
+    let url = config.get_str("host.url").unwrap();
+    println!("url: {}", url);
+    if url.is_empty(){
+        eprintln!("Please set url in config/config.toml");
+        return;
+    }
+    match download_hosts_file(&url).await {
         Ok(content) => {
             match append_to_hosts_file(&content) {
                 Ok(()) => println!("Hosts file updated successfully!"),
